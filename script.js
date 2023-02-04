3ec292e45f9e3d2807a841ac86753386574885b7
DESKTOP-62NBI5U\yshkw <yshkwi.chacha@gmail.com>
2023-02-05 06:59:06 +0900

    ｚいんでっくすに翻弄されている

--- script.js
+++ script.js
@@ -1,7 +1,140 @@
-$(function(){
+$(function () {
 
 
-    $(".slide-items").slick();
+    // $(".slide-items").slick();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // やすはのぞーん
+    // お役立ちサイト
+    $(".site-circle").on("mouseover",function () {
+        console.log($(".site-circle"));
+        $(this).addClass("on");
+    })
+    $(".site-circle").on("mouseleave",function () {
+        console.log($(".site-circle"));
+        $(this).removeClass("on");
+    })
+
+    // topへスクロール
+    $(".go-to-top").on("click",function () {
+        $("body,html").animate({scrollTop:0},500)
+    })
+
+    // お問い合わせボタン
+    $(".info-btn").hover(function () {
+        $(this).toggleClass("on")
+    })
+    $(".info-btn").on("click",function () {
+        $(".info-container").addClass('on')
+    })
+    $(".close-btn").on("click",function () {
+        $(".info-container").removeClass('on')
+    })
+
+
 
 
 })
